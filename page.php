cffb0c673a1f087951807ac316f6d9f40d4948c4
KC <omega128@gmail.com>
2015-10-16 22:56:13 -0700

    Added single and page support

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+	get_header();
+?>
+<main>
+	<?php
+		while ( have_posts() ) : the_post();
+			get_template_part( 'content', 'page' );
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		endwhile;
+		?>
+</main>
+<?php
+	get_footer();
+?>
