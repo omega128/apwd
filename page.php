cd8095dd7850e91a77240528defbf515bdc74307
KC <omega128@gmail.com>
2015-10-16 23:01:24 -0700

    Fixed content bug on page.php

--- page.php
+++ page.php
@@ -4,12 +4,12 @@
 <main>
 	<?php
 		while ( have_posts() ) : the_post();
-			get_template_part( 'content', 'page' );
+			<?php the_content(); ?>
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 		endwhile;
-		?>
+	?>
 </main>
 <?php
 	get_footer();
