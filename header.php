a26ae5fb6fbf11fd5293d1c3a3927ebeb0c81fcb
KC <omega128@gmail.com>
2015-11-05 15:35:10 -0800

    Simplifies menu CSS

--- header.php
+++ header.php
@@ -13,25 +13,8 @@
     </header>
     <nav>
 <?php
-$defaults = array(
-	'theme_location'  => '',
-	'menu'            => '',
-	'container'       => 'div',
-	'container_class' => '',
-	'container_id'    => '',
-	'menu_class'      => 'menu',
-	'menu_id'         => 'main_menu',
-	'echo'            => true,
-	'fallback_cb'     => 'wp_page_menu',
-	'before'          => '',
-	'after'           => '',
-	'link_before'     => '',
-	'link_after'      => '',
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-	'depth'           => 0,
-	'walker'          => ''
-);
-wp_nav_menu( $defaults );
+
+wp_nav_menu( array('menu_id' => 'main_menu') );
 ?>
         <!-- wp_list_pages('sort_column=menu_order&depth=1&title_li='); -->
     </nav>
