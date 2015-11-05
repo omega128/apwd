105ae1b253fa417715ce4b04adc4b4466dec3d9a
KC <omega128@gmail.com>
2015-11-05 15:18:52 -0800

    Adds menu support

--- header.php
+++ header.php
@@ -13,6 +13,30 @@
     </header>
     <nav>
       <ul>
-        <?php wp_list_pages('sort_column=menu_order&depth=1&title_li='); ?>
+<?php
+
+$defaults = array(
+	'theme_location'  => '',
+	'menu'            => '',
+	'container'       => 'div',
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => 'menu',
+	'menu_id'         => 'main_menu',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	'depth'           => 0,
+	'walker'          => ''
+);
+
+wp_nav_menu( $defaults );
+
+?>
+        <!-- wp_list_pages('sort_column=menu_order&depth=1&title_li='); -->
       </ul>
     </nav>
