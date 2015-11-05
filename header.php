316fbd41e42ff43ed64a33ec9817f9657cf3eec5
KC <omega128@gmail.com>
2015-11-05 02:17:44 -0800

    Fixes pointer to stylesheet URL

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
   <div id="wrapper">
