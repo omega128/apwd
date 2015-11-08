791d12a41c97a21435423f3b299cd1364dd45591
KC <omega128@gmail.com>
2015-11-08 14:56:16 -0800

    Streamlines page layout

--- front-page.php
+++ front-page.php
@@ -3,14 +3,9 @@
 ?>
     <main>
       <?php while ( have_posts() ) : the_post(); ?>
-        <section class="header">
-          <h1><?php the_title(); ?></h1>
-        </section>
-
       <section class="content">
         <?php the_content(); ?>
       </section>
-
       <?php endwhile; ?>
     </main>
 <?php
--- page.php
+++ page.php
@@ -2,11 +2,11 @@
   get_header();
 ?>
     <main>
-      <section class="header">
-        <h2>Index</h2>
-      </section>
-
       <?php while ( have_posts() ) : the_post(); ?>
+        <section class="header">
+          <h1><?php the_title(); ?></h1>
+        </section>
+
       <div id="breadcrumbs">
         <?php if ( function_exists('yoast_breadcrumb')) {
     yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
@@ -14,8 +14,6 @@
     ?></div>
 
       <section class="content">
-        <h2><?php the_title(); ?></h2>
-        <h3><?php the_time('F jS, Y') ?></h3>
         <?php the_content(); ?>
       </section>
       <?php endwhile; ?>
