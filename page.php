32693a9cc6b935ce340d3bd47e89f591b4729d27
KC <omega128@gmail.com>
2015-11-08 15:41:26 -0800

    Removes sidebar

--- page.php
+++ page.php
@@ -13,20 +13,8 @@
   }
     ?></div>
 
-      <?php
-        if (get_pages('child_of=' . $post->ID)) {
-          // if this page has subpages, display a sidebar with a list of them
-          echo '<aside id="left_sidebar"><ul>';
-          wp_list_pages('depth=1&title_li=&child_of='.$post->ID);
-          echo '</ul></aside>';
-          echo '<section class="right_content">';
-        }
-        else {
-          // this page does not have subpages, use a single section with no sidebar
-          echo '<section class="content">';
-        }
-      ?>
-      <?php the_content(); ?>
+      <section class="content">
+        <?php the_content(); ?>
       </section>
       <?php endwhile; ?>
     </main>
