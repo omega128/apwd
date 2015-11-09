a992bbb0de87c2dca4684b520168f59467a7b786
KC <omega128@gmail.com>
2015-11-08 17:20:40 -0800

    Added aligncenter

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <ul>
           <li><a href=""><img src="http://placehold.it/32x32" alt="Staff"/> Staff</a></li>
           <li><a href=""><img src="http://placehold.it/32x32" alt="Sitemap"/> SiteMap</a></li>
-          <li><a href=""><img src="http://placehold.it/32x32" alt="Contact"/> Contact</a></li>
+          <li><a href="http://ec2-52-26-188-113.us-west-2.compute.amazonaws.com/apwd/contact/"><img src="http://placehold.it/32x32" alt="Contact"/> Contact</a></li>
         </ul>
       </div>
       <div class="grid_4">
