2ab37a4a1abe636899912e7a721121747fadd5a2
KC <omega128@gmail.com>
2015-11-20 21:26:33 -0800

    Redivides footer into thirds instead of fourths

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="sitemap">
-      <div class="grid_4">
+      <div class="grid_3">
         <ul>
           <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/staff.png" alt="Staff" width="40"/> Staff</a></li>
           <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sitemap.png" alt="Sitemap" width="40"/> SiteMap</a></li>
@@ -7,14 +7,14 @@
 <?php echo get_stylesheet_directory_uri(); ?>/images/contact.png" alt="Contact" width="40"/> Contact</a></li>
         </ul>
       </div>
-      <div class="grid_4">
+      <div class="grid_3">
         <h2>Location</h2>
         <p>111.222.3333</p>
         <p>example@mail.com</p>
         <p>12345 Avenue, Suite 333</p>
         <p>Santa Axure, SA, 98765</p>
       </div>
-      <div class="grid_2">
+      <div class="grid_3">
         <h2>Accessibility Options</h2>
       </div>
     </div>
