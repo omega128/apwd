e175ffa55b59c46f6841993d73fa79ef4b717528
KC <omega128@gmail.com>
2015-12-08 15:39:39 -0800

    Added login/logout

--- sidebar-footer1.php
+++ sidebar-footer1.php
@@ -2,7 +2,7 @@
   <?php dynamic_sidebar('footer1'); ?>
 <?php else : ?>
 <ul>
-  <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/staff.png" alt="Staff" width="40"/> Staff</a></li>
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/staff.png" alt="Staff" width="40"/> <?php wp_loginout(); ?> </li>
   <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sitemap.png" alt="Sitemap" width="40"/> SiteMap</a></li>
   <li><a href="http://ec2-52-26-188-113.us-west-2.compute.amazonaws.com/apwd/contact/"><img src="
 <?php echo get_stylesheet_directory_uri(); ?>/images/contact.png" alt="Contact" width="40"/> Contact</a></li>
